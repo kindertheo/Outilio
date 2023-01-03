45e29c64e588014bb8cc0992247cc40d624af9a7
Théo <kinder.theo@gmail.com>
2023-01-03 16:25:31 +0100

    add step 2 and 4

--- resources/js/modules/orders/components/OrderForm.vue
+++ resources/js/modules/orders/components/OrderForm.vue
@@ -1,79 +1,107 @@
 <template>
-    <form>
+    <div>
         Etape 1
         <v-date-picker v-model="order.date">
             <template v-slot="{ inputValue, togglePopover }">
                 <div class="flex items-center">
-                    <button
-                        class=""
-                        @click="togglePopover()"
-                    >
-                       <i class="fa fa-calendar"></i>
+                    <button class="" @click="togglePopover()">
+                        <i class="fa fa-calendar"></i>
                     </button>
-                    <input
-                        :value="inputValue"
-                        readonly
-                    />
+                    <input :value="inputValue" readonly />
                 </div>
             </template>
         </v-date-picker>
 
-        Etape 3
-        <label>Nom</label>
-        <input type="text" v-model="order.customerLastname"/>
-
-        <label>Prénom</label>
-        <input type="text" v-model="order.customerFirstname"/>
-
-        <label>Téléphone</label>
-        <input type="text" v-model="order.customerPhone"/>
-
-        <label>Email</label>
-        <input type="text" v-model="order.customerEmail"/>
-
-        <label>Livraison</label>
-        <div class="form-check">
-            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" :value="false" v-model="order.deliveryOption">
-            <label class="form-check-label" for="flexRadioDefault1">
-                Livraison gratuite
-            </label>
-        </div>
-        <div class="form-check">
-            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" :value="true" v-model="order.deliveryOption">
-            <label class="form-check-label" for="flexRadioDefault2">
-                Livraison payante
-            </label>
+        <div>
+            Etape 2 <br />
+            Une réduction est appliquée dès 2 outils selectionnés
+            <products-list />
         </div>
 
-        <button type="button" @click="createOrder">
-            Valider
-        </button>
-    </form>
+        <div>
+            Etape 3
+            <label>Nom</label>
+            <input type="text" v-model="order.customerLastname" />
+
+            <label>Prénom</label>
+            <input type="text" v-model="order.customerFirstname" />
+
+            <label>Téléphone</label>
+            <input type="text" v-model="order.customerPhone" />
+
+            <label>Email</label>
+            <input type="text" v-model="order.customerEmail" />
+
+            <label>Livraison</label>
+            <div class="form-check">
+                <input
+                    class="form-check-input"
+                    type="radio"
+                    name="flexRadioDefault"
+                    id="flexRadioDefault1"
+                    :value="false"
+                    v-model="order.deliveryOption"
+                />
+                <label class="form-check-label" for="flexRadioDefault1">
+                    Livraison gratuite
+                </label>
+            </div>
+            <div class="form-check">
+                <input
+                    class="form-check-input"
+                    type="radio"
+                    name="flexRadioDefault"
+                    id="flexRadioDefault2"
+                    :value="true"
+                    v-model="order.deliveryOption"
+                />
+                <label class="form-check-label" for="flexRadioDefault2">
+                    Livraison payante
+                </label>
+            </div>
+        </div>
+
+        <div>
+            Etape 4 - Récapitulatif
+
+            <div v-for="product in order.products" :key="product.id">
+                {{ product.name }} {{ product.price_by_day / 100 }}
+            </div>
+
+            <div>Réduction : Montant total : {{ this.getOrderPrice }}</div>
+            <button type="button" @click="createOrder" :disabled="this.isFormInvalid">Valider</button>
+        </div>
+    </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
+import { mapGetters, mapActions } from "vuex";
+import { product } from "../../products/services/product";
 
 export default {
     name: "OrderForm",
     computed: {
-        ...mapGetters('OrderStore', [
-            'getOrder'
-        ]),
+        ...mapGetters("OrderStore", ["getOrder", "getOrderPrice"]),
         order: {
-            get(){
+            get() {
                 return this.getOrder;
-            }
-        }
+            },
+        },
+        isFormInvalid() {
+            return this.order.customerLastname === "" ||
+                this.order.customerFirstname === "" ||
+                this.order.customerPhone === "" ||
+                this.order.customerEmail === "" ||
+                this.order.products.length === 0
+                ? true
+                : false;
+        },
     },
     methods: {
-        ...mapActions('OrderStore', [
-            'createOrder'
-        ])
-    }
-}
+        ...mapActions("OrderStore", ["createOrder"]),
+    },
+    components: { product },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
