c49dce65852b858fbb80ffed118b567f328bfe19
Théo <kinder.theo@gmail.com>
2023-01-03 16:50:49 +0100

    add productview to comopnent

--- resources/js/services/components.js
+++ resources/js/services/components.js
@@ -2,22 +2,26 @@ import lastStringElement from "../utils/lastStringElement";
 import kebabize from "../utils/kebabize";
 
 const components = [
-    'orders/OrdersView',
-    'orders/components/OrderForm',
-    'products/components/ProductCard',
-    'products/components/ProductsList',
-    'products/components/ProductForm'
+    "orders/OrdersView",
+    "orders/components/OrderForm",
+    "products/components/ProductCard",
+    "products/components/ProductsList",
+    "products/components/ProductForm",
+    "products/ProductView",
 ];
 
 const addComponent = (app, component) => {
-    const componentName = lastStringElement(component, '/');
-    app.component(kebabize(componentName), require(`../modules/${component}`).default);
-}
+    const componentName = lastStringElement(component, "/");
+    app.component(
+        kebabize(componentName),
+        require(`../modules/${component}`).default
+    );
+};
 
 const addComponents = (app) => {
-    for(const component of components){
+    for (const component of components) {
         addComponent(app, component);
     }
-}
+};
 
 export { addComponents };
