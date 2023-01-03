b9e0765d42cbc200a2ac9dce89a585d99c7120f3
Théo <kinder.theo@gmail.com>
2023-01-03 16:26:09 +0100

    calcul price

--- resources/js/modules/orders/services/order.js
+++ resources/js/modules/orders/services/order.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import getSum from "../../../utils/getSum";
+import getPrice from "../../../utils/getPrice";
 
 export const order = {
     namespaced: true,
@@ -7,33 +9,46 @@ export const order = {
         order: {
             date: new Date(),
             deliveryOption: false,
-            customerPhone: '',
-            customerEmail: '',
-            customerFirstname: '',
-            customerLastname: '',
-            price: 0,
-            productsIds: []
+            customerPhone: "",
+            customerEmail: "",
+            customerFirstname: "",
+            customerLastname: "",
+            products: [],
         },
     }),
-    mutations: {
-
-    },
+    mutations: {},
     actions: {
-        async createOrder({dispatch, getters}){
-            const order = getters.getOrder;
+        async createOrder({ dispatch, getters }) {
+            let order = getters.getOrder;
 
-            const { data } = await axios.post('/orders', {
-                order
+            order.price = getters.computedPrice;
+
+            const { data } = await axios.post("/orders", {
+                order,
             });
-
         },
-        async updateOrder({dispatch, getters}){
-
-        }
+        async updateOrder({ dispatch, getters }) {},
     },
     getters: {
-        getOrder(state){
+        getOrder(state) {
             return state.order;
-        }
-    }
-}
+        },
+        computedPrice(state){
+            const price = getSum(
+                state.order.products.map((product) => product.price_by_day)
+            );
+
+            const deliveryPrice = state.order.deliveryOption ? 2000 : 0;
+
+            if (state.order.products.length < 2) return price + deliveryPrice;
+
+            return (
+                price * (1 - (state.order.products.length * 5) / 100) +
+                deliveryPrice
+            );
+        },
+        getOrderPrice(state, getters) {
+           return getPrice(getters.computedPrice);
+        },
+    },
+};
