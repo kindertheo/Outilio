368dde3fb5f1c2d2018eacd0390579f19a776763
Théo <kinder.theo@gmail.com>
2023-01-03 16:50:15 +0100

    starting edit product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -81,7 +81,9 @@ class ProductController extends Controller
      */
     public function edit(Product $product)
     {
-        //
+        return view('products.edit', [
+            'product' => $product
+        ]);
     }
 
     /**
