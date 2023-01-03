bac007b1bdf5649ad3ee7e3201face284c0c0dbe
Théo <kinder.theo@gmail.com>
2023-01-03 15:02:06 +0100

    create route about products

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [OrderController::class, 'create']);
 Route::get('/orders', [OrderController::class, 'index']);
 Route::post('/orders', [OrderController::class, 'store']);
 Route::put('/orders', [OrderController::class, 'update']);
+
+Route::get('/products', [ProductController::class, 'create']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/all-products', [ProductController::class, 'all']);
