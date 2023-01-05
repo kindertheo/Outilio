8c73d5c1a4231d6cb719506fdd6ecf76a275d46c
Selim <zouache.selim57@gmail.com>
2023-01-05 09:46:16 +0100

    add tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,10 +3,15 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Support\Str;
-
+use Illuminate\Support\Facades\Session;
+use Mockery;
+use Mockery\MockInterface;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Cache;
 
 class ExampleTest extends TestCase
 {
@@ -15,15 +20,79 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function test_the_application_returns_a_successful_response()
+    
+    public function test_the_homepage_returns_a_successful_response()
     {
+        
         $response = $this->get('/');
         $response->assertStatus(200);
-        var_dump($response);
+       
        
     }
 
-    public function testBasicTest()
+    public function test_the_productpage_returns_a_successful_response()
+    {
+        $response = $this->get('/products');
+        $response->assertStatus(200);   
+    }
+
+
+    public function test_insert_data_in_table_product()
+    {
+        //Création Mock
+            // $this->instance(
+            //     ProductController::class,
+            //     Mockery::mock(ProductController::class, function (MockInterface $mock) {
+            //         $mock->shouldReceive('store')->once();
+            // Cache::spy();
+            Bus::fake([
+                ProductController::class,
+                Product::class
+            ]);
+            Session::start();
+            $token = csrf_token();
+            dump($token);
+            // Action
+            $response = $this->withHeaders([
+                'X-CSRF-TOKEN'=>$token
+            ])
+            ->postJson('/products', [
+                'product'=>[
+                    'name'=>'perceuse',
+                    'description'=>'test',
+                    'priceByDay'=>"52"
+                ]
+            ]);
+            // $response->dump();
+            // Assertions
+            $response2 = $this->getJson("/products/30");
+            $response->assertOk();
+            $response2->assertOk();
+        //     })
+        // );
+    }
+
+    public function test_retreave_first_data_in_table_product()
+    {
+        // Assert that each user in the list has at least an id, name and email attribute.
+        $response = $this->assertDatabaseHas('products', [
+                'id'=> 1,
+                'name'=>'perceuse',
+                'description'=>'coucou',
+                'price_by_day'=> 4500,
+                'created_at'=> "2023-01-04 08:48:34",
+                'updated_at'=> "2023-01-04 08:48:34"
+
+        ]);
+    }
+
+    /*public function test_assert_database_count()
+    {
+        $response = $this->assertDatabaseCount('products', 26);
+    }*/
+
+
+    /*public function testBasicTest()
     {
         $data = [10, 20, 30];
         $result = array_sum($data);
@@ -37,13 +106,8 @@ class ExampleTest extends TestCase
         $this->assertSame(Str::startsWith($data, 'Tu'), false);
         $this->assertStringStartsWith('Je', $data);
         $this->assertStringEndsWith('petit', $data);
-    }
-    /*
-    public function testBasicTest3()
-    {
-        $response = $this->get('/');
-        $response->assertSuccessful();
-        $this->assertEquals('coucou', $response->getContent());
     }*/
+    
+   
     
 }
