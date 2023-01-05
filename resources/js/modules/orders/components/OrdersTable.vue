635fe8b399bc425ed1a96446d7a9478f3344a4e8
IC-faycal <faycal@inovea-conseil.com>
2023-01-05 14:04:19 +0100

    ADD table order manage

--- resources/js/modules/orders/components/OrdersTable.vue
+++ resources/js/modules/orders/components/OrdersTable.vue
@@ -1,38 +1,45 @@
 <template>
-    <table class="table table-striped table-hover" style="height: 80vh">
-        <thead>
-            <td class="p-3">Client</td>
-            <td class="p-3">Email</td>
-            <td class="p-3">Tél.</td>
-            <td class="p-3">Outils</td>
-            <td class="p-3">Prix</td>
-            <td class="p-3">Date</td>
-        </thead>
-        <tbody>
-            <tr>
-                <td colspan="7" class="fw-bold" style="color: #D80000">
-                    A traiter
-                </td>
-            </tr>
-            <order-row v-for="order of this.notProcessedOrder" :order="order" />
-        </tbody>
-        <tbody>
-            <tr>
-                <td colspan="7" class="fw-bold" style="color: #FFA012">
-                    Non terminées
-                </td>
-            </tr>
-            <order-row v-for="order of this.notEndedOrders" :order="order" />
-        </tbody>
-        <tbody>
-        <tr>
-            <td colspan="7" class="fw-bold" style="color: #009621">
-                Terminées
-            </td>
-        </tr>
-        <order-row v-for="order of this.endedOrders" :order="order" />
-        </tbody>
-    </table>
+    <div class="container" style="height: 82vh">
+        <div class="table-responsive mt-4">
+            <table class="table table-light table-sm m-10px table-hover">
+                <thead>
+                    <tr>
+                        <th class="p-3">Client</th>
+                        <th class="p-3">Email</th>
+                        <th class="p-3">Tél.</th>
+                        <th class="p-3">Outils</th>
+                        <th class="p-3">Prix</th>
+                        <th class="p-3">Date</th>
+                        <th class="p-3">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td colspan="7" class="fw-bold" style="color: #D80000; height: 18px">
+                            A traiter
+                        </td>
+                    </tr>
+                    <order-row v-for="order of this.notProcessedOrder" :order="order" />
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td colspan="7" class="fw-bold" style="color: #FFA012; height: 18px !important;">
+                            Non terminées
+                        </td>
+                    </tr>
+                    <order-row v-for="order of this.notEndedOrders" :order="order" />
+                </tbody>
+                <tbody>
+                    <tr>
+                        <td colspan="7" class="fw-bold" style="color: #009621; height: 18px">
+                            Terminées
+                        </td>
+                    </tr>
+                    <order-row v-for="order of this.endedOrders" :order="order" />
+                </tbody>
+            </table>
+        </div>
+    </div>
 </template>
 
 <script>
