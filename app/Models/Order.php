626fa06f61826c4925f56b3b12e5794c9fb49443
IC-faycal <faycal@inovea-conseil.com>
2023-01-06 11:35:11 +0100

    WIP send mail

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,7 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Order extends Model
+class Order extends Model implements \Illuminate\Contracts\Mail\Mailable
 {
     use HasFactory;
 
